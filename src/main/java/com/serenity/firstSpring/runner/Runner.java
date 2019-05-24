db286cce886917df0705fdb0fe9d40641a6b788d
Ahmed Samy <serenitydiver@hotmail.com>
2019-05-24 11:37:08 +0300

    Initial commit

---
+++ src/main/java/com/serenity/firstSpring/runner/Runner.java
@@ -0,0 +1,17 @@
+package com.serenity.firstSpring.runner;
+
+import com.serenity.firstSpring.bean.Person;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Person person = applicationContext.getBean("person", Person.class);
+        System.out.println("Person id: " + person.getId() + ", Person Name: " + person.getName());
+
+        Person person1 = applicationContext.getBean("person1", Person.class);
+        System.out.println("Person id One: " + person1.getId() + ", Person Name One: " + person1.getName());
+
+    }
+}
